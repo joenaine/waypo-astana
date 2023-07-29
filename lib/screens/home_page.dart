c372cf91f84bb987e6d31f3e46d73380b4ad653e
joenaine <joenaine10@gmail.com>
2023-07-30 02:33:10 +0600

    [ADD] 'animation duration'

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -55,7 +55,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                 children: singleCircleList.map((e) => e).toList(),
               ),
               const SizedBox(height: 20),
-              const Text('Animation '),
+              const Text('Animation duration'),
               AnimatedBuilder(
                   animation: animationController,
                   builder: (_, __) {
